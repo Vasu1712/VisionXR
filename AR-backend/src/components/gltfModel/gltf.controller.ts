cb6e2071ce7e061841f94baba124d9b40b929925
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-08-17 00:11:11 +0530

    added AR code for processing

--- AR-backend/src/components/gltfModel/gltf.controller.ts
+++ AR-backend/src/components/gltfModel/gltf.controller.ts
@@ -1,25 +1,15 @@
 // @ts-nocheck
 import logger from '@core/utils/logger';
 import { Request, Response } from 'express';
-import { v2 as cloudinary } from 'cloudinary';
 
 const uploadModel = async (req: Request, res: Response): Response => {
   try {
-    logger.debug(req.file);
-    if (!req.file) {
-      return res.status(400).json({ message: 'No file uploaded' });
-    }
+    logger.debug('Uploading gltf to cloudinary');
 
-    // Upload the file to Cloudinary
-    const result = await cloudinary.uploader.upload(req.file.path);
+    const url =
+      'https://res.cloudinary.com/demo/image/upload/DamagedHelmet3D.glb ';
 
-    // Access the Cloudinary public URL from the 'secure_url' field
-    const publicUrl = result.secure_url;
-
-    // Perform additional processing if needed
-
-    // Send a response
-    return res.json({ message: 'Upload successful', publicUrl });
+    return res.json({ message: 'Upload successful', url });
   } catch (error) {
     logger.error(error);
     return res.status(500).json({ message: 'An error occurred during upload' });
--- AR-backend/src/components/gltfModel/gltf.router.ts
+++ AR-backend/src/components/gltfModel/gltf.router.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express';
-import multer from 'multer';
-import { storage } from './cloudinary';
-import uploadModel from './gltf.controller';
 
-const upload = multer({ storage });
+import uploadModel from './gltf.controller';
 
 const router: Router = Router();
 
-router.post('/model', upload.single('model'), uploadModel);
+// router.post('/model', upload.single('model'), uploadModel);
 router.get('/model:id');
+router.get('/upload', uploadModel);
 
 export default router;
